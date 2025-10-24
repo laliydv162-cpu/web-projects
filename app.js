6ff1ae2e8becd7646091ae05eb45a7ccdebb9802
laliydv162-cpu <laliydv162@gmail.com>
2025-10-24 15:07:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = input.value;
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "Delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    input.value = "";
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName=='BUTTON'){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("delete");
+    }
+});
